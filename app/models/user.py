b5fac85faf1245badcc5846baeac3b5a152fb6d8
Shibnath Hansda <196810131+Hero24-x@users.noreply.github.com>
2026-06-30 20:33:23 +0530

    Add User model for user authentication

---
+++ app/models/user.py
@@ -0,0 +1,22 @@
+from app.core.extensions import db
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model):
+
+    __tablename__ = "users"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+    username = db.Column(
+        db.String(100),
+        unique=True,
+        nullable=False
+    )
+
+    password = db.Column(
+        db.String(255),
+        nullable=False
+    )
